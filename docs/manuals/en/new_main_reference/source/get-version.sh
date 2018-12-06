59092456bab2798f96e42e7b4bc4ce5af8d33354
Philipp Storz <philipp.storz@bareos.com>
2018-12-06 10:28:39 +0100

    fix

--- docs/manuals/en/new_main_reference/source/get-version.sh
+++ docs/manuals/en/new_main_reference/source/get-version.sh
@@ -4,5 +4,5 @@ VERSION_H=../../../../../core/src/include/version.h
 if [ -f ${VERSION_H}  ]; then
   grep "^#define VERSION" ${VERSION_H} | cut -b 17-  | sed 's/"//g'
 else
-  dpkg-parsechangelog  | grep Version | cut -b 10-
+  cd ../../../../ && dpkg-parsechangelog  | grep Version | cut -b 10-
 fi
